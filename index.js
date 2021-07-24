950f0898eb563f0e7c124adedf61b9b021e136bc
jfullerco <jonathan@jfuller.co>
2021-07-23 22:15:53 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const puppeteer = require('puppeteer')
+
+const url = process.argv[2]
+!url ? "Please provide a URL" : url
+
+const run = async() => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({path: 'screenshot.png'})
+  browser.close()
+}
+
+run()
